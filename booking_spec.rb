73fbe2fd9f4410dfd40a1ed001cb8f7936f6a9ff
Jason Swett <jason@benfranklinlabs.com>
2018-05-18 09:52:00 -0400

    Refactoring.

--- booking_spec.rb
+++ booking_spec.rb
@@ -3,9 +3,10 @@ require './hotel'
 require './booking'
 
 describe Booking do
+  let(:hotel) { Hotel.new([100, 101, 102, 103]) }
+
   describe 'when all rooms are vacant' do
     it 'assigns the first room number' do
-      hotel = Hotel.new([100, 101, 102, 103])
       booking = hotel.check_in_guest('George Harrison')
       expect(booking.room_number).to eq(100)
     end
@@ -13,8 +14,6 @@ describe Booking do
 
   describe 'when one room is occupied' do
     it 'assigns the next room number' do
-      hotel = Hotel.new([100, 101, 102, 103])
-
       first_booking = hotel.check_in_guest('George Harrison')
       second_booking = hotel.check_in_guest('Roy Orbison')
 
